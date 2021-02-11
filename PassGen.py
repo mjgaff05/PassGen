22426d605a2d8bed326060ee3c376a625a0619f1
mjgaff05 <mighty96mjg@yahoo.com>
2021-02-10 18:05:47 -0600

    Project setup

---
+++ PassGen.py
@@ -0,0 +1,13 @@
+import random
+
+#Function to take user requirements
+
+#Function to ask user whether to make up random chars or input strings
+
+#Function to take user strings
+
+
+
+#Output password
+password = "Not_secure101"
+print(password)
