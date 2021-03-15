5c1fff4239fc0d16db777876f0a7db173db54251
mjgaff05 <mighty96mjg@yahoo.com>
2021-03-14 19:09:47 -0500

    Added random gen of characters based on user selected character sets.

--- PassGen.py
+++ PassGen.py
@@ -2,6 +2,23 @@ import random
 import sys
 
 #Function to take user requirements
+def getCharSet():
+	input_set = input("Do you want to include numbers or special characters?\n1) Numbers\n2) Special\n").split(",")
+	print(input_set)
+
+	set_bools = [0,0]
+	for x in input_set:
+ 		if x == '1':
+ 			set_bools[0] = 1
+ 		elif x == '2':
+ 			set_bools[1] = 1
+ 		else:
+ 			print("Invalid Set")
+
+	print(set_bools)
+	return set_bools
+
+
 
 #Take first requirement which is length of password (should be an integer)
 try:
@@ -11,32 +28,12 @@ except ValueError:
 	sys.exit(0)
 
 #Take requirements in upper case, lower case, special chars, numbers, etc
-input_set = input("Enter the corresponding numbers of character sets to include, separated by a comma.\n1) Numbers\n2) Upper Case\n3) Lower Case\n4) Special\n").split(",")
-print(input_set)
-
+sets = getCharSet()
 numbers = "0123456789"
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 special = "!#$%&()+<=>?@^~"
 
-
-char_set = ""
-chars = ""
-num = 0
-uc = 0
-lc = 0
-spec = 0
-for x in input_set:
- 	if x == '1':
- 		num = 1
- 	elif x == '2':
- 		uc = 1
- 	elif x == '3':
- 		lc = 1
- 	elif x == '4':
- 		spec = 1
- 	else:
- 		char_set = "Invalid Set"
 
 #Function to ask user whether to make up random chars or input strings
 #input_text = input("Do you want to enter a word to use in the password? (Y/N) ")
@@ -50,15 +47,24 @@ for x in input_set:
 
 #Get characters for the password
 pass_chars = ""
-if spec == 1:
+if sets[1] == 1:
 	pass_chars = pass_chars + random.choice(special) + random.choice(special)
-if num == 1:
+if sets[0] == 1:
 	pass_chars = pass_chars + random.choice(numbers) + random.choice(numbers)
 for x in range(len(pass_chars), num_digits):
 	pass_chars = pass_chars + random.choice(upper_case + lower_case)
-	
+print(pass_chars)
+
 #Mix up the password
+loc = 0
+password = ""
+loc_set = []
+for i in range(0, len(pass_chars)):
+	loc = random.choice([i for i in range(0, (len(pass_chars))) if i not in loc_set])
+	loc_set.append(loc)
+	password = password + pass_chars[loc]
+
 
 #Output password
-password = pass_chars
 print(password)
+
